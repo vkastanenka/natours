50f2d9ad85efcde294e3a28cefe63e452274a674
Victoria Kastanenka <vkastanenka@gmail.com>
2020-05-02 19:32:25 -0400

    TourCardOverview props established

--- client/src/pages/ToursOverview/Layout/TourCardOverview.js
+++ client/src/pages/ToursOverview/Layout/TourCardOverview.js
@@ -1,64 +1,72 @@
 // React
 import React from "react";
+import PropTypes from 'prop-types';
 
 // Components
 import Icon from "../../../components/Icon/Icon";
 
-const TourCardOverview = () => {
+const TourCardOverview = (props) => {
   return (
     <div className="card">
       <div className="card__header">
         <div className="card__picture">
-          <div className="card__picture-overlay">
-            &nbsp;
-          </div>
-          <img
-              src={require("../../../assets/images/tours/tour-1-cover.jpg")}
-              alt="Tour 1"
-              className="card__picture-img"
-            />
+          <div className="card__picture-overlay">&nbsp;</div>
+          {props.cover}
         </div>
         <h3 className="card__heading">
-          <span>The Forest Hiker</span>
+          <span>{props.name}</span>
         </h3>
       </div>
 
       <div className="card__details">
-        <h4 className="card__sub-heading">Easy 5 day tour</h4>
-        <p className="card__text">
-          Breathtaking hike through the Canadian Banff National Park
-        </p>
+        <h4 className="card__sub-heading">{props.duration}</h4>
+        <p className="card__text">{props.description}</p>
         <div className="card__data">
           <Icon type="map-pin" className="card__icon" />
-          <span>Banff, Canada</span>
+          <span>{props.location}</span>
         </div>
         <div className="card__data">
           <Icon type="calendar" className="card__icon" />
-          <span>April, 2021</span>
+          <span>{props.date}</span>
         </div>
         <div className="card__data">
           <Icon type="flag" className="card__icon" />
-          <span>3 stops</span>
+          <span>{props.stops}</span>
         </div>
         <div className="card__data">
           <Icon type="user" className="card__icon" />
-          <span>25 people</span>
+          <span>{props.participants}</span>
         </div>
       </div>
 
       <div className="card__footer">
         <p>
-          <span className="card__footer-value">$297</span>
+          <span className="card__footer-value">{`$${props.price}`}</span>
           <span className="card__footer-text"> per person</span>
         </p>
         <p className="card__ratings">
-          <span className="card__footer-value">4.9</span>
-          <span className="card__footer-text"> rating (21)</span>
+          <span className="card__footer-value">{props.ratingValue}</span>
+          <span className="card__footer-text">{` rating (${props.ratingCount})`}</span>
         </p>
+        {/* TODO: Click to redirect to /tour/tour-name */}
         <button className="btn btn--green btn--small">Details</button>
       </div>
     </div>
   );
 };
+
+TourCardOverview.propTypes = {
+  cover: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  duration: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  stops: PropTypes.string.isRequired,
+  participants: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  ratingValue: PropTypes.string.isRequired,
+  ratingCount: PropTypes.string.isRequired,
+}
 
 export default TourCardOverview;
--- client/src/pages/ToursOverview/ToursOverview.js
+++ client/src/pages/ToursOverview/ToursOverview.js
@@ -6,16 +6,96 @@ import PageLayout from "../../components/Layout/PageLayout";
 import TourCardOverview from "./Layout/TourCardOverview";
 
 const ToursOverview = () => {
+  const coverForestHiker = (
+    <img
+      src={require("../../assets/images/tours/tour-1-cover.jpg")}
+      alt="Tour 1"
+      className="card__picture-img"
+    />
+  );
+
   return (
     <PageLayout>
       <main className="main">
         <div className="card-container">
-          <TourCardOverview />
-          <TourCardOverview />
-          <TourCardOverview />
-          <TourCardOverview />
-          <TourCardOverview />
-          <TourCardOverview />
+          <TourCardOverview
+            cover={coverForestHiker}
+            name='The Forest Hiker'
+            duration='Easy 5 day tour'
+            description='Breathtaking hike through the Canadian Banff National Park'
+            location='Banff, Canada'
+            date='April, 2021'
+            stops='3 stops'
+            participants='25 people'
+            price='297'
+            ratingValue='4.9'
+            ratingCount='21'
+          />
+          <TourCardOverview
+            cover={coverForestHiker}
+            name='The Forest Hiker'
+            duration='Easy 5 day tour'
+            description='Breathtaking hike through the Canadian Banff National Park'
+            location='Banff, Canada'
+            date='April, 2021'
+            stops='3 stops'
+            participants='25 people'
+            price='297'
+            ratingValue='4.9'
+            ratingCount='21'
+          />
+          <TourCardOverview
+            cover={coverForestHiker}
+            name='The Forest Hiker'
+            duration='Easy 5 day tour'
+            description='Breathtaking hike through the Canadian Banff National Park'
+            location='Banff, Canada'
+            date='April, 2021'
+            stops='3 stops'
+            participants='25 people'
+            price='297'
+            ratingValue='4.9'
+            ratingCount='21'
+          />
+          <TourCardOverview
+            cover={coverForestHiker}
+            name='The Forest Hiker'
+            duration='Easy 5 day tour'
+            description='Breathtaking hike through the Canadian Banff National Park'
+            location='Banff, Canada'
+            date='April, 2021'
+            stops='3 stops'
+            participants='25 people'
+            price='297'
+            ratingValue='4.9'
+            ratingCount='21'
+          />
+          <TourCardOverview
+            cover={coverForestHiker}
+            name='The Forest Hiker'
+            duration='Easy 5 day tour'
+            description='Breathtaking hike through the Canadian Banff National Park'
+            location='Banff, Canada'
+            date='April, 2021'
+            stops='3 stops'
+            participants='25 people'
+            price='297'
+            ratingValue='4.9'
+            ratingCount='21'
+          />
+          <TourCardOverview
+            cover={coverForestHiker}
+            name='The Forest Hiker'
+            duration='Easy 5 day tour'
+            description='Breathtaking hike through the Canadian Banff National Park'
+            location='Banff, Canada'
+            date='April, 2021'
+            stops='3 stops'
+            participants='25 people'
+            price='297'
+            ratingValue='4.9'
+            ratingCount='21'
+          />
         </div>
       </main>
     </PageLayout>
