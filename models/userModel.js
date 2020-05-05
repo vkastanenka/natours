dec4cf61a424317c056042644941e7a8a7a7e25b
Victoria Kastanenka <vkastanenka@gmail.com>
2020-05-05 14:41:07 -0400

    updated user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,44 +1,44 @@
 const crypto = require("crypto");
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please tell us your name"]
+    required: [true, "Please tell us your name"],
   },
   email: {
     type: String,
     required: [true, "Please provide your email"],
     unique: true,
     lowercase: true,
-    validate: [validator.isEmail]
+    validate: [validator.isEmail],
   },
   photo: {
     type: String,
-    default: 'default.jpg'
+    default: "default.jpg",
   },
   role: {
     type: String,
     enum: ["user", "guide", "lead-guide", "admin"],
-    default: "user"
+    default: "user",
   },
   password: {
     type: String,
     required: [true, "Please provide a password"],
     minlength: 8,
-    select: false
+    select: false,
   },
   passwordConfirm: {
     type: String,
     required: [true, "Please confirm your password"],
     validate: {
-      validator: function(el) {
+      validator: function (el) {
         return el === this.password;
       },
-      message: "Passwords must match"
-    }
+      message: "Passwords must match",
+    },
   },
   passwordChangedAt: Date,
   passwordResetToken: String,
@@ -46,70 +46,80 @@ const userSchema = new mongoose.Schema({
   active: {
     type: Boolean,
     default: true,
-    select: false
-  }
+    select: false,
+  },
 });
 
-// Password encryption will happen between the moment where it's actually persisted to the database
-userSchema.pre("save", async function(next) {
-  // Do not want to re-encrpt password if not being updated
-  // .isModified() is a method we have on all documents which we can use is a certain field has been modified
-  // If password has NOT been modified, return from this function
+//////////////////////
+// Document Middleware
+
+// Pre-Save Password Encrpytion
+userSchema.pre("save", async function (next) {
+  // 1. If password isn't modified, continue
   if (!this.isModified("password")) return next();
 
+  // 2. If password IS modified, encrypt it
   this.password = await bcrypt.hash(this.password, 12);
-
-  // Do not persist passwordConfirm to the database (only needed for above validation)
   this.passwordConfirm = undefined;
   next();
 });
 
-userSchema.pre("save", function(next) {
-  // If the document is new or password hsan't been modified, simply return
+// Pre-Save Recording of Password Change Time
+userSchema.pre("save", function (next) {
+  // 1. If the document is new or password hasn't been modified, return
   if (!this.isModified("password") || this.isNew) return next();
 
-  // Sometimes, saving to the database is a bit slower than issuing the JSON web token => Makes it so that the changed password timestamp is sometimes set a bit after the JWT has been created => User won't be able to log in using the new token (fix by subtracting 1 second)
+  // 2. Record the time the password was changed at
   this.passwordChangedAt = Date.now() - 1000;
   next();
 });
 
-userSchema.pre(/^find/, function(next) {
-  // this points to the current query => Will only show users whose active state is set to true
+///////////////////
+// Query Middleware
+
+// Pre-find filter for user's who have their active state set to false
+userSchema.pre(/^find/, function (next) {
+  // Find users who don't have their active state set to false
   this.find({ active: { $ne: false } });
   next();
 });
 
-// Instance method => Will be available on all documents of a certain collection
-userSchema.methods.correctPassword = async function(
+////////////////////
+// Instance Methods
+
+// bcrypt Password Comparison => Returns true or false based on candidate and user passwords
+userSchema.methods.correctPassword = async function (
   candidatePassword,
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-userSchema.methods.changedPasswordAfter = async function(JWTTimestamp) {
+// JWT Timestamp Validation Against passwordChangedAt Field
+userSchema.methods.changedPasswordAfter = async function (JWTTimestamp) {
   if (this.passwordChangedAt) {
-    // Make both timestamps equivalent formats
+    // 1. Make sure the JWT is not expired
     const changedTimestamp = parseInt(
       this.passwordChangedAt.getTime() / 1000,
       10
     );
-    return JWTTimestamp < changedTimestamp; // 100 < 200: true
+    return JWTTimestamp < changedTimestamp;
   }
 
-  // False means password hasn't been changed
+  // 2. False means password hasn't been changed
   return false;
 };
 
-userSchema.methods.createPasswordResetToken = function() {
-  // Password reset token should be a random string => Does not need to be as cryptographically strong as the password hash
+// Creating Password Reset Token
+userSchema.methods.createPasswordResetToken = function () {
+  // 1. Create token and assign it to the user document
   const resetToken = crypto.randomBytes(32).toString("hex");
   this.passwordResetToken = crypto
     .createHash("sha256")
     .update(resetToken)
     .digest("hex");
 
-  // Will expire in 10 minutes
+  // 2. 10 minute expiration
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
   return resetToken;
