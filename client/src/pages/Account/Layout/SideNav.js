2094476a3c3ac26736ed0a0abde7f53656fb841a
Victoria Kastanenka <vkastanenka@gmail.com>
2020-05-08 17:14:25 -0400

    changed account page so only admin and lead-guides can view admin options

--- client/src/pages/Account/Layout/SideNav.js
+++ client/src/pages/Account/Layout/SideNav.js
@@ -1,42 +1,66 @@
 // React
 import React from "react";
+import PropTypes from "prop-types";
+
+// Redux
+import { connect } from "react-redux";
 
 // Components
 import Icon from "../../../components/Icon/Icon";
 
-const SideNav = () => {
+const SideNav = (props) => {
+  console.log(props.auth.user);
+
   return (
     <nav className="user-view__menu">
       <ul className="side-nav">
         <li className="side-nav--active">
-          <Icon type="settings" className='side-nav__icon' />Settings
+          <Icon type="settings" className="side-nav__icon" />
+          Settings
         </li>
         <li>
-          <Icon type="briefcase" className='side-nav__icon' />My bookings
+          <Icon type="briefcase" className="side-nav__icon" />
+          My bookings
         </li>
         <li>
-          <Icon type="star" className='side-nav__icon' />My reviews
+          <Icon type="star" className="side-nav__icon" />
+          My reviews
         </li>
         <li>
-          <Icon type="credit-card" className='side-nav__icon' />Billing
+          <Icon type="credit-card" className="side-nav__icon" />
+          Billing
         </li>
       </ul>
-      <div className="admin-nav">
-        <h5 className="admin-nav__heading">Admin</h5>
-        <ul className="side-nav">
-          <li>
-            <Icon type="map" className='side-nav__icon' />Manage tours
-          </li>
-          <li>
-            <Icon type="users" className='side-nav__icon' />Manage users
-          </li>
-          <li>
-            <Icon type="star" className='side-nav__icon' />Manage reviews
-          </li>
-        </ul>
-      </div>
+      {props.auth.user.role === "admin" ||
+      props.auth.user.role === "lead-guide" ? (
+        <div className="admin-nav">
+          <h5 className="admin-nav__heading">Admin</h5>
+          <ul className="side-nav">
+            <li>
+              <Icon type="map" className="side-nav__icon" />
+              Manage tours
+            </li>
+            <li>
+              <Icon type="users" className="side-nav__icon" />
+              Manage users
+            </li>
+            <li>
+              <Icon type="star" className="side-nav__icon" />
+              Manage reviews
+            </li>
+          </ul>
+        </div>
+      ) : null}
     </nav>
   );
 };
 
-export default SideNav;
+SideNav.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(SideNav);
