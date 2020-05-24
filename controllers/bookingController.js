b72ded4585cb8f7693f5537ac56d49723ec6e494
Victoria Kastanenka <vkastanenka@gmail.com>
2020-05-24 18:42:39 -0400

    small url fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -32,7 +32,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-    success_url: `${req.protocol}://localhost:3000/account/bookings`,
+    success_url: `${req.protocol}://${req.get("host")}/account/bookings`,
     cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
