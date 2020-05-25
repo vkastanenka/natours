560e890b69a655019d1933b7c501dbd5af7d43e2
Victoria Kastanenka <vkastanenka@gmail.com>
2020-05-25 15:00:36 -0400

    new path

--- app.js
+++ app.js
@@ -97,9 +97,9 @@ app.use("/api/v1/bookings", bookingRouter);
 
 
 // Serving static files
-app.use('/static', express.static(path.join(__dirname, "client/build")));
+app.use('/static', express.static(path.join(__dirname, "./client/build")));
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // Handling Unhandled Routes
