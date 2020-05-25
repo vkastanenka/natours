1627592736e044efb2a84ad396b2849647a1a22d
Victoria Kastanenka <vkastanenka@gmail.com>
2020-05-25 16:43:46 -0400

    attempting to fix redux devtools

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -12,11 +12,13 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(applyMiddleware(...middleware))
-  // compose(
-  //   applyMiddleware(...middleware),
-  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  // )
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
+  )
 );
 
 export default store;
